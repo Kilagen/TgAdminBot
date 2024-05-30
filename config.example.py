217fd8334b93970123ada70a38dd7655493b6e2a
Kirill Zakharov <kil4gen@gmail.com>
2024-05-30 14:48:08 +0300

    add config example

--- config.example.py
+++ config.example.py
@@ -1,2 +1,39 @@
-FORWARD_SPAM_CHAT_ID = -1
+import json
+import typing as tp
+from dataclasses import dataclass
+
 BOT_TOKEN = "1234567890:ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+
+
+@dataclass
+class ChatData:
+    id: int
+    redirect_to: int
+    admins: tp.List[int]
+
+
+@dataclass
+class Config:
+    general_admins: tp.List[int]
+    chats: tp.List[ChatData]
+
+
+def file_to_config(path='config.json') -> Config:
+    config_data = json.load(open(path, 'r'))
+    general_admins = config_data['general_admins']
+    chats = config_data['chats']
+    config = Config(general_admins, [])
+    for chat in chats:
+        config.chats.append(ChatData(chat['id'], chat['redirect_to'], chat['admins']))
+    return config
+
+
+def config_to_file(config: Config, path='config.json') -> None:
+    mapping = {
+        'general_admins': config.general_admins,
+        'chats': [{'id': chat.id, 'redirect_to': chat.redirect_to, 'admins': chat.admins} for chat in config.chats]
+    }
+    json.dump(mapping, open(path, 'w'))
+
+
+config = file_to_config()
